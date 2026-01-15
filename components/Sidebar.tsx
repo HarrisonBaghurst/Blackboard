0853f7fcdd5f557005f488a4e28513ee80e07553
Harrison Baghurst <070806harrison@gmail.com>
2026-01-15 16:44:13 +0000

    removed stretching from images in side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { motion } from 'framer-motion';
 import Image from "next/image";
 import { useCurrentColourRef } from "@/contexts/ToolContext";
+import { cn } from "@/lib/utils";
 
 const Sidebar = () => {
     const tools: ([string, number, string])[] = [
@@ -17,7 +18,10 @@ const Sidebar = () => {
 
     return (
         <div
-            className='fixed right-[1%] top-1/2 -translate-y-1/2 card-style'
+            className={cn(
+                'fixed right-[1%] top-1/2 -translate-y-1/2 card-style flex flex-col gap-(--padding) p-(--padding) items-end',
+                'w-[calc(110px+var(--padding)*2)]'
+            )}
         >
             {tools.map((tool, index) => (
                 <motion.div
@@ -26,9 +30,9 @@ const Sidebar = () => {
                         setCurrentTool(index);
                         currentColourRef.current = tool[2];
                     }}
-                    className="h-15 w-35 cursor-pointer origin-right overflow-hidden"
+                    className="h-15 w-[110px] relative cursor-pointer overflow-hidden p-(--padding)"
                     animate={{
-                        scaleX: currentTool === index ? 1.25 : 1
+                        width: currentTool === index ? 145 : 110
                     }}
                     transition={{
                         type: 'spring',
@@ -36,18 +40,18 @@ const Sidebar = () => {
                         damping: 25,
                     }}
                 >
-                    {tool[0] !== '' ?
-                        (
-                            <Image
-                                src={tool[0]}
-                                width={0}
-                                height={0}
-                                alt="tool"
-                                style={{ transform: `rotate(${tool[1]}deg)` }}
-                                className='w-full h-full scale-270'
-                            />
-                        ) : (<></>)
-                    }
+                    <div className="absolute top-1/2 -translate-y-1/2 left-0 w-[145px] h-15">
+                        <Image
+                            src={tool[0]}
+                            alt="tool"
+                            width={0}
+                            height={0}
+                            className="w-full h-full scale-220"
+                            style={{
+                                rotate: `${tool[1]}deg`
+                            }}
+                        />
+                    </div>
                 </motion.div>
             ))}
         </div>
