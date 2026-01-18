46ef85f3ba89547a09172731729a190b65bb2fd3
Harrison Baghurst <070806harrison@gmail.com>
2026-01-18 20:48:09 +0000

    added basic home page and board setup

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,27 @@
-import Board from '@/components/Board'
-import { Room } from './Room'
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { v4 as uuidv4 } from 'uuid'
 
 const page = () => {
+    const router = useRouter();
+
+    const createBoard = () => {
+        const id = uuidv4();
+        router.push(`/board/${id}`)
+    }
+
     return (
-        <Room>
-            <Board />
-        </Room>
+        <div className='p-4'>
+            <button
+                className='card-style px-4 py-2 cursor-pointer'
+                onClick={createBoard}
+            >
+                Create Board
+            </button>
+        </div>
     )
+
 }
 
 export default page
---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,12 @@
+import Board from '@/components/Board'
+import { Room } from './Room'
+
+const page = () => {
+    return (
+        <Room>
+            <Board />
+        </Room>
+    )
+}
+
+export default page
